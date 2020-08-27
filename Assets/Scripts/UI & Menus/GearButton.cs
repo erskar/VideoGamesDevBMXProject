5e17be8d0c102dc5be125b1a44d8cb8317b16c45
Jordan S <jordan.stoj000@gmail.com>
2020-08-27 16:06:01 +1000

    Added UI to gear menu and more UI to the game (non-functional)

--- Assets/Scripts/UI & Menus/GearButton.cs
+++ Assets/Scripts/UI & Menus/GearButton.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GearButton : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
